a06f0007a9395330fbcb27f43717f2e67fa5f37e
quaK <38787176+Joelrau@users.noreply.github.com>
2021-10-31 09:26:02 +0200

    Fix xsurface dumping to mw3

--- src/IW4/Assets/XSurface.cpp
+++ src/IW4/Assets/XSurface.cpp
@@ -229,7 +229,7 @@ namespace ZoneTool
 			for (auto i = 0u; i < asset->numsurfs; i++)
 			{
 				dump.dump_int(asset->surfs[i].tileMode);
-				dump.dump_int(asset->surfs[i].deformed);
+				dump.dump_int(asset->surfs[i].deformed ? 64 : 0);
 				dump.dump_int(asset->surfs[i].baseTriIndex);
 				dump.dump_int(asset->surfs[i].baseVertIndex);
 
